48fe301c71b6abd471de9d66050d404dd6756d1b
Thibault Hazelart <thazelart@gmail.com>
2020-08-25 14:04:50 +0200

    Use rtd grey logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,7 +53,7 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_logo = '_static/terraform-validator.svg'
+html_logo = '_static/terraform-validator-rtd.svg'
 html_favicon = '_static/terraform-validator-icon.svg'
 html_theme_options = {
     'logo_only': True,
